4ad53d72367f8ceda3100a7a05f7048598946d3c
fuguel <nyx@fuguel.xyz>
2026-02-17 14:33:55 -0500

    copied hosts files into folder and chown it to user group

---
+++ hosts/gbook/configuration.nix
@@ -0,0 +1,90 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports =
+    [ 
+      ./hardware-configuration.nix
+    ];
+       nix.settings.experimental-features = [ "nix-command" "flakes" ];
+ 
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  hardware.graphics.enable = true;
+
+
+
+  networking.hostName = "gbook"; 
+  # Configure network connections interactively with nmcli or nmtui.
+  networking.networkmanager.enable = true;
+  time.timeZone = "America/New_York";
+  
+  # Configure network proxy if necessary
+  # Select internationalisation properties.
+   i18n.defaultLocale = "en_US.UTF-8";
+   console = {
+   font = "Lat2-Terminus16";
+ #   keyMap = "us";
+   useXkbConfig = true; 
+   };
+  
+
+  # Enable the X11 windowing system.
+  # services.xserver.enable = true;
+  
+  # Enable sound.
+  # services.pulseaudio.enable = true;
+  # OR
+  # services.pipewire = {
+  #   enable = true;
+  #   pulse.enable = true;
+  # };
+  
+  # Enable touchpad support (enabled default in most desktopManager).
+    services.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+      users.users.nyx = {
+      isNormalUser = true;
+      extraGroups = [ "wheel" "networkmanager" ]; 
+                    };  
+      environment.systemPackages = with pkgs; [ 
+      glances
+      fastfetch
+      tmux  
+      gh
+      git
+      wget
+      htop
+      cage
+      kitty
+      curl 
+      qutebrowser
+];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+  
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
+  system.stateVersion = "25.11"; 
+ }
+

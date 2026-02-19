b360da098f2fd1bf289ea616baefb91416a65f3f
fuguel <nyx@fuguel.xyz>
2026-02-19 14:54:01 -0500

    added kitty to common

--- common/common.nix
+++ common/common.nix
@@ -11,6 +11,7 @@
                      git
                      gh
                      fastfetch 
+                     kitty
                      glances
                      cage
                      curl

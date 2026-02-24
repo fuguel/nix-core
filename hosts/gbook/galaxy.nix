3c48cb57588bedd219aa4be91c17f6f37e06829e
fuguel <nyx@fuguel.xyz>
2026-02-24 14:18:13 -0500

    Enabled Pipewire

--- hosts/gbook/galaxy.nix
+++ hosts/gbook/galaxy.nix
@@ -25,16 +25,13 @@
   boot.kernelPackages = pkgs.linuxPackages_latest;
   hardware.xpadneo.enable = true;
   hardware.steam-hardware.enable = true;
+  hardware.bluetooth.enable = true; 
  
- 
- hardware.graphics = {
-         enable = true;
-         enable32Bit = true;
-                     };
- 
-
-
-  networking.hostName = "gbook"; 
+  hardware.graphics = {
+           enable = true;
+           enable32Bit = true;
+                      };
+    networking.hostName = "gbook"; 
   # Configure network connections interactively with nmcli or nmtui.
   networking.networkmanager.enable = true;
   # Configure network proxy if necessary
@@ -44,10 +41,10 @@
   # Enable sound.
   # services.pulseaudio.enable = true;
   # OR
-  # services.pipewire = {
-  #   enable = true;
-  #   pulse.enable = true;
-  # };
+   services.pipewire = {
+   enable = true;
+   pulse.enable = true;
+                       };
   # Enable touchpad support (enabled default in most desktopManager).
     services.libinput.enable = true;  
   # Some programs need SUID wrappers, can be configured further or are
@@ -69,8 +66,6 @@
   # (/run/current-system/configuration.nix). This is useful in case you
   # accidentally delete configuration.nix.
   # system.copySystemConfiguration = true;
-
-  hardware.bluetooth.enable = true;
   system.stateVersion = "25.11"; 
  }
 

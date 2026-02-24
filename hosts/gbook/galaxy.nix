c33f03ba48782968b351a94974288d7c84b0f820
fuguel <nyx@fuguel.xyz>
2026-02-24 12:22:43 -0500

    cleaned up xpad config

--- hosts/gbook/galaxy.nix
+++ hosts/gbook/galaxy.nix
@@ -23,8 +23,6 @@
   boot.loader.efi.canTouchEfiVariables = true;
   
   boot.kernelPackages = pkgs.linuxPackages_latest;
-  boot.extraModulePackages = [ config.boot.kernelPackages.xpadneo ];
-  boot.kernelModules = [ "hid-xpadneo" ];
   hardware.xpadneo.enable = true;
   hardware.steam-hardware.enable = true;
  

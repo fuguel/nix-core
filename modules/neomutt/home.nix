ad2bfacb96d3da980980795a00e512ae167c41ae
fuguel <nyx@fuguel.xyz>
2026-02-27 10:40:44 -0500

    setting up nemutt mail folder and directory

--- modules/neomutt/home.nix
+++ modules/neomutt/home.nix
@@ -2,9 +2,21 @@
 {
   programs.neomutt = {
       enable = true;
+  
+      extraConfig = ''
+        set realname = "Jand"
+        set from = "nyx@fuguel.xyz"
+
+        set folder = "~/mail"
+        set spoolfile = "+INBOX"
+        set mbox_type = "Maildir"
+
+        set sendmail = "${pkgs.msmtp}/bin/msmtp"
+       '';
   };
 
-  home.packages = with pkgs; [
+
+home.packages = with pkgs; [
          isync
          msmtp
          w3m

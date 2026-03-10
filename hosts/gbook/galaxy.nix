69a4d01e864a87a8174aa0ba0753fb7a00b41fe5
fuguel <nyx@fuguel.xyz>
2026-03-10 13:10:58 -0400

    added blacklisted kernel modules and changed log level, acpi errors continue on gbook

--- hosts/gbook/galaxy.nix
+++ hosts/gbook/galaxy.nix
@@ -31,9 +31,9 @@ environment.systemPackages = with pkgs; [
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  
+  boot.blacklistedKernelModules = [ "samsung_laptop" ];
   boot.kernelPackages = pkgs.linuxPackages_latest;
-  boot.kernelParams = [ "loglevel=3" "pci-nommconf" "ucsi_acpi.ignore_errors=1" "systemd.show_status=true" "rd.udev.log_level=3" "acpi_enforce_resources=lax" ];
+  boot.kernelParams = [ "loglevel=0" "pci-nommconf" "ucsi_acpi.ignore_errors=1" "systemd.show_status=true" "rd.udev.log_level=3" "acpi_enforce_resources=lax" "acpi_osi=" ];
   hardware.xpadneo.enable = true;
   hardware.steam-hardware.enable = true;
   hardware.bluetooth.enable = true; 

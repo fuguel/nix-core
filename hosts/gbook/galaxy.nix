cef479752ec918d4404a9d684166e8048c0d35b2
fuguel <nyx@fuguel.xyz>
2026-03-11 13:30:04 -0400

    masked the actual kernel module and set all logs to zero for acpi gbook

--- hosts/gbook/galaxy.nix
+++ hosts/gbook/galaxy.nix
@@ -32,9 +32,9 @@ environment.systemPackages = with pkgs; [
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelPackages = pkgs.linuxPackages_latest;
-  boot.kernelParams = [ "loglevel=0" "pci-nommconf" "ucsi_acpi.ignore_errors=1" "systemd.show_status=true" "rd.udev.log_level=3" "acpi_enforce_resources=lax" "modprobe.blacklist=acpi_fan" ];
+  boot.kernelParams = [ "loglevel=0" "pci-nommconf" "ucsi_acpi.ignore_errors=1" "systemd.show_status=true" "rd.udev.log_level=0" "acpi_enforce_resources=lax" "modprobe.blacklist=acpi_fan" "acpi_mask_gpe=0x4B"
+                         "udev.log_level=0" ];
   boot.blacklistedKernelModules = [ "acpi_fan" ];
-  boot.initrd.blacklistedKernelModules = [ "acpi_fan" ];
   hardware.xpadneo.enable = true;
   hardware.steam-hardware.enable = true;
   hardware.bluetooth.enable = true; 

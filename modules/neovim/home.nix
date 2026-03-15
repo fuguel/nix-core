4d17ea8413f133c063aef3ba95cbd6c0b4d10309
fuguel <nyx@fuguel.xyz>
2026-03-15 14:36:58 -0400

    home manger owner of bash shell for environmental editor variables

--- modules/neovim/home.nix
+++ modules/neovim/home.nix
@@ -4,6 +4,12 @@
    enable = true;
    defaultEditor = true;
    };
+ home.sessionVariables = {
+      EDITOR = "nvim";
+      VISUAL = "nvim";
+  };
+programs.bash.enable = true;
+
 
  xdg.configFile."nvim/init.lua".source = ./init.lua;
 }
